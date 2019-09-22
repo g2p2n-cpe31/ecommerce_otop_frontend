266fc3b573e659c33ae9db67eeced8f9d97a9cc8
poonsht <poon_arsene_lupin@hotmail.com>
2019-09-22 12:13:18 +0700

    [+] add : add resources for reuse importing

---
+++ src/components/resources/Colors.js
@@ -0,0 +1,10 @@
+const colors = {
+  text: '#000000', // black
+  black_87: 'rgba(0,0,0, 0.87)',
+  black_8: 'rgba(0,0,0, 0.8)',
+  black_7: 'rgba(0,0,0, 0.7)',
+  black_6: 'rgba(0,0,0, 0.6)',
+  black_34: 'rgba(0,0,0, 0.34)',
+  black_2: 'rgba(0,0,0,.2)',
+}
+export default colors
---
+++ src/components/resources/R.js
@@ -0,0 +1,5 @@
+import colors from './colors'
+const R = {
+  colors,
+}
+export default R
