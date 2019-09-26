811049b64fdfe682d2cca043190229d37e200b5c
khpierce <pierce_iii@hotmail.com>
2019-09-26 18:15:16 +0700

    p2

---
+++ src/pages/cart.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const ContainerSection = styled.div`
+    width: 100%;
+    height: 464px;
+    background: linear-gradient(326.77deg, rgba(232, 156, 107, 0.4) 0%, rgba(247, 227, 127, 0.4) 100%);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+const ContainerText = styled.div`
+    display: flex;
+    width: 80%;
+`
+const TextSession = styled.p`
+    font-family: Kanit;
+    font-size: 3rem;
+    line-height: 43px;
+`
+
+const Cart = () => {
+    return (
+        <ContainerSection>
+            <ContainerText>
+                <TextSession>สินค้าแนะนำ</TextSession>
+            </ContainerText>
+        </ContainerSection>
+    )
+}
+
+export default Cart
