dc59fbdd44dbb2d004429a12c58b701e449223b9
Jarawee THONGCHAROEN <jarawee.t@ku.th>
2019-10-27 00:54:18 +0700

    [+] create page sell

---
+++ src/pages/sell.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import styled from 'styled-components'
+import Layout from '../components/common/Layout'
+import Button from '@material-ui/core/Button'
+import Join from '../components/containers/sell/Join'
+import Create from '../components/containers/sell/Create'
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+    button: {
+      margin: theme.spacing(1),
+    },
+    input: {
+      display: 'none',
+    },
+  }));
+
+
+const ContainerMyStore = styled(Button)`
+    &&{
+        width:100%;
+        height: 16.8rem;
+        background: #FFFFFF;
+        margin-top: 23.5rem;
+    }
+`
+const StoreName = styled.p`
+    font-family: Kanit;
+    font-size: 3.6rem;
+    margin-bottom: 0;
+    margin-top: 0;
+`
+
+const ContainerCreate = styled.div`
+    display: flex;
+    flex-direction: row;
+`
+
+
+
+const Sell = () => {
+    const classes = useStyles();
+    return(
+        <Layout>
+            <ContainerMyStore disabled className={classes.button}>
+                <StoreName>
+                    ร้านค้าของฉัน
+                </StoreName>
+            </ContainerMyStore>
+            <ContainerCreate>
+                <Create/>
+                <Join/>
+            </ContainerCreate>
+        </Layout>
+    )
+}
+export default Sell
