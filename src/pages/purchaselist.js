f49817cfe3f612f10a5becfb7c5f14edbc5b2d6c
kanlaya <gunlaya1998@hotmail.com>
2019-11-05 20:00:50 +0700

    [+] save

--- src/pages/purchaselist.js
+++ src/pages/purchaselist.js
@@ -1,14 +1,9 @@
 import React from 'react'
 // import SEO from '../components/utility/seo'
-import styled from 'styled-components'
+// import styled from 'styled-components'
 // import { QuantityProvider } from '../components/context/QuantityProduct'
 import PurchaseOrder from '../components/containers/PurchaseOrder/PurchaseOrder.js'
 import Layout from '../components/common/Layout'
-
-const Container = styled.div`
-  margin: 0 0;
-`
-
 
 const PurchaseList = ({ location ,history}) => {
     return (
